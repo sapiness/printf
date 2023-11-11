d0aa7b325ba84c98f4f04ea1bfaa2fed9ae52d9f
mikoiyke <mikoiyke@gmail.com>
2023-11-11 11:02:19 -0800

    task_0.c

--- task_0.c
+++ task_0.c
@@ -1,41 +1,50 @@
 #include "main.h"
 
+/**
+  * _printf - jjjj
+  * @format: kkk
+  * Return: 0
+  */
 int _printf(const char *format, ...)
 {
 	int i;
-    va_list args;
-    va_start(args, format);
+	va_list args;
 
-    if (format == NULL)
-    {
-        va_end(args);
-        return -1; /*You may want to choose a proper error code*/
-    }
+	va_start(args, format);
 
-    for (i = 0; format[i] != '\0'; i++)
-    {
-        if (format[i] == '%')
-        {
-            i++;
-            if (format[i] == 'c')
-            {
-                char c = va_arg(args, int); /*Read the next argument as a char*/
-                write(1, &c, 1);
-            }
-            else if (format[i] == 's')
-            {
-                char *str = va_arg(args, char *); /*Read the next argument as a string*/
-                write(1, str, strlen(str));
-            }
-            /*Add more cases for other format specifiers as needed*/
-        }
-        else
-        {
-            write(1, &format[i], 1);
-        }
-    }
+	if (format == NULL)
+	{
+		va_end(args);
+		return (-1); /*You may want to choose a proper error code*/
+	}
 
-    va_end(args);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == 'c')
+			{
+				char c = va_arg(args, int); /*Read the next argument as a char*/
 
-    return 0; /*Return success or an appropriate value*/
+				write(1, &c, 1);
+			}
+			else if (format[i] == 's')
+			{
+				char *str = va_arg(args, char *); /*Read the next argument as a string*/
+
+				write(1, str, strlen(str));
+			}
+
+
+		}
+		else
+		{
+			write(1, &format[i], 1);
+		}
+	}
+
+	va_end(args);
+
+	return (0); /*Return success or an appropriate value*/
 }
