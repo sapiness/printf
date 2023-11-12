097d5bf329a814067c1e6b446a4dd9303b6ae1d7
mikoiyke <mikoiyke@gmail.com>
2023-11-12 03:17:34 -0800

    task-0.c

--- task-0.c
+++ task-0.c
@@ -1,25 +1,22 @@
 #include "main.h"
 
 /**
-  * _printf - Entry
-  * @format: KKK
-  * Return: success
+  * _printf - entry point
+  * @format: JJ
+  * Return: LL
   */
 
 int _printf(const char *format, ...)
 {
+	unsigned int i;
 	va_list args;
+
 	va_start(args, format);
 
 	if (format == NULL)
-	{
-		va_end(args);
 		return (-1);
-	}
 
-	int count = 0;
-
-	for (int i = 0; format[i] != '\0'; i++)
+	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] == '%')
 		{
@@ -29,29 +26,25 @@ int _printf(const char *format, ...)
 				char c = va_arg(args, int);
 
 				write(1, &c, 1);
-				count++;
 			}
 			else if (format[i] == 's')
 			{
 				char *str = va_arg(args, char *);
 
 				write(1, str, strlen(str));
-				count += strlen(str);
 			}
 			else if (format[i] == '%')
 			{
 				write(1, "%", 1);
-				count++;
 			}
 		}
 		else
 		{
 			write(1, &format[i], 1);
-			count++;
 		}
 	}
 
 	va_end(args);
 
-	return (count);
+	return (0);
 }
