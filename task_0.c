613d53bd36765686b852b44e8d9931f0bc944d4a
mikoiyke <mikoiyke@gmail.com>
2023-11-12 21:55:39 -0800

    task_0.c

--- task_0.c
+++ task_0.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 int _printf(const char *format, ...) {
-  va_list args;
+
+	int i,j;
+	int count = 0;
+	va_list args;
   va_start(args, format);
 
   if (format == NULL) {
     return -1;
   }
 
-  int count = 0;
-
-  for (int i = 0; format[i] != '\0'; i++) {
+  for (i = 0; format[i] != '\0'; i++) {
     if (format[i] == '%') {
       i++;
       switch (format[i]) {
@@ -23,11 +24,15 @@ int _printf(const char *format, ...) {
         case 's': {
           char *str = va_arg(args, char *);
           if (str != NULL) {
-            _fputs(str, stdout);
-            count += _strlen(str);
+            for (j = 0; str[j] != '\0'; j++) {
+              _putchar(str[j]);
+              count++;
+            }
           } else {
-            _fputs("(null)", stdout);
-            count += 6;
+            for (j = 0; "(null)"[j] != '\0'; j++) {
+              _putchar("(null)"[j]);
+              count++;
+            }
           }
           break;
         }
