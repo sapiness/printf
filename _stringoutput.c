fdaead235265f5fa4ae8ea2f164657f3c7f44a1a
mikoiyke <mikoiyke@gmail.com>
2023-11-13 08:57:32 -0800

    _stringoutput.c

--- _stringoutput.c
+++ _stringoutput.c
@@ -2,11 +2,12 @@
 
 /**
   * _stringoutput - print the entire string   * str: string charaters
-  * return: the entire string
+  * @str: character
+  * Return: the entire string
   */
 
 int _stringoutput(char *str)
 {
 	write(1, str, _strlen(str));
- 	return(_strlen(str));
+	return (_strlen(str));
 }
