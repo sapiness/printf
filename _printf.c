40455190fc63640be1cafec334bb92217d4ce0bd
mikoiyke <mikoiyke@gmail.com>
2023-11-14 13:05:02 -0800

    this task is sth else

--- _printf.c
+++ _printf.c
@@ -1,24 +1,20 @@
 #include "main.h"
-
 /**
  * _printf - prints formatted string
  * @format: format string
  * Return: number of characters printed (excluding null byte)
  */
-
 int _printf(const char *format, ...)
 {
 	va_list args;
 	int i, len = 0;
+	char *str;
 
 	va_start(args, format);
-
 	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
-
 	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
-
 	for (i = 0; format[i]; i++)
 	{
 		if (format[i] == '%')
@@ -27,7 +23,13 @@ int _printf(const char *format, ...)
 			if (format[i] == 'c')
 				len += _putchar(va_arg(args, int));
 			else if (format[i] == 's')
-				len += _stringoutput(va_arg(args, char *));
+			{
+				str = va_arg(args, char *);
+				if (str == NULL)
+					len += write(1, "(null)", 6);
+				else
+					len += _stringoutput(str);
+			}
 			else if (format[i] == '%')
 				len += _putchar('%');
 			else
@@ -42,7 +44,6 @@ int _printf(const char *format, ...)
 			len++;
 		}
 	}
-
 	va_end(args);
 	return (len);
 }
