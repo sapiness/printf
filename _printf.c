6a081f71bded63ae3bb91e675a604de05d430563
sapiness <sapiness695@gmail.com>
2023-11-15 02:41:42 -0800

    Task 1 is not working at all

--- _printf.c
+++ _printf.c
@@ -1,5 +1,42 @@
 #include "main.h"
 /**
+ * _printf_numbers - prints an integer value
+ * description: printing
+ * @number: jjj
+ * Return: hhhh
+ */
+int _printf_numbers(int number)
+{
+	int copy, digit, divider = 1, digitCount = 0;
+
+	if (number < 0)
+	{
+		_putchar('-');
+		number = -number;
+	}
+
+	copy = number;
+
+	while (copy >= 10)
+	{
+		divider *= 10;
+		copy /= 10;
+	}
+
+	while (divider > 0)
+	{
+		digit = number / divider;
+		_putchar(digit + '0');
+		number %= divider;
+		divider /= 10;
+		digitCount++;
+	}
+
+	return (digitCount);
+}
+
+#include "main.h"
+/**
  * _printf - prints formatted string
  * @format: format string
  * Return: number of characters printed (excluding null byte)
