8bfa0ed9fb16f6af5efddac29586e0b817ac2826
sapiness <sapiness695@gmail.com>
2023-11-14 16:52:19 -0800

    fixing task 1

---
+++ _printf2.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+int _printf(int number)
+{
+    int copy, divider = 1, digitCount = 0;
+
+    if (number == INT_MIN)
+    {
+        _putchar('-');
+        number = -(number + 1);
+    }
+    else if (number < 0)
+    {
+        _putchar('-');
+        number = -number;
+    }
+
+    (copy = number);
+
+    while (copy >= 10)
+    {
+        divider *= 10;
+        copy /= 10;
+    }
+
+    while (divider > 0)
+    {
+        int digit = number / divider;
+        _putchar(digit + '0');
+        number %= divider;
+        divider /= 10;
+        digitCount++;
+    }
+
+    return (digitCount);
+}
